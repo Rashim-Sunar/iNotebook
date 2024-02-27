80b490f3a9ad611e775a1e14a712aae417840fdc
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-02-26 20:27:18 -0500

    Updating an existing Note on mongodb

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose')
 const {Schema} = mongoose
 
 const noteSchema = new Schema({
-    user : {type : mongoose.Schema.Types.ObjectId , ref : "user"}, //Links user with notes
+    user : {type : mongoose.Schema.Types.ObjectId , ref : "user"}, //Links user with notes, ref:"collection-name"
     title : {type:String , required:true},
     description : {type:String},
-    tag : {type:String,default:'General'}
+    tag : {type:String,default:'General'},
+    date : {type:Date , default:Date.now()}
 })
 
 module.exports = mongoose.model('note',noteSchema);
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -36,6 +36,35 @@ router.post('/addnotes',fetchUser,[
         console.log(error.message);
         res.status(500).json({error:"Internal Server Error"})
     }
+});
 
-})
+//ROUTE-3 : Update an existing note using : PUT"api/notes/updatenotes". Login required
+router.put('/updatenotes/:id',fetchUser,async(req,res)=>{
+    try{
+    const {title , description , tag} = req.body;
+    //Create a newNote object
+    const newNote = {}
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    //Find the note to be updated and then update it
+    let note =await Notes.findById(req.params.id)    //req.params.id is the id fetched from /updatenotes/:id<--
+    if(!note){
+        return res.status(404).send("Not Found");
+    }
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Access denied!");
+    }
+
+    note = await Notes.findByIdAndUpdate(req.params.id , {$set : newNote} , {new:true});
+    res.json({note});
+    }catch(error){
+    console.log(error.message);
+    res.status(400).json({error : "Internal Server Error"});
+    }
+
+});
+
 module.exports = router
